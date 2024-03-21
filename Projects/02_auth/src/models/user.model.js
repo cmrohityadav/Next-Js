bccd1765021bcb878e1d25ec8b603a0f35b0ad7a
cmrohityadav <cmrohityadav23@gmail.com>
2024-03-21 21:04:19 +0530

    user model

---
+++ Projects/02_auth/src/models/user.model.js
@@ -0,0 +1,40 @@
+import mongoose, { Schema } from "mongoose"
+const userSchema=new Schema({
+    username:{
+        type:String,
+        required:[true,"Please Provide Username"],
+        unique:[true,"username shloud be unique"],
+
+
+    },
+    email:{
+        type:String,
+        required:[true,"Please Provide email"],
+        unique:[true,"email shloud be unique"],
+        
+
+    },
+    password:{
+        type:String,
+        required:[true,"Please Provide password"],
+    },
+    isVerified:{
+        type:Boolean,
+        default:false
+        
+    },
+    isAdmin:{
+        type:Boolean,
+        default:false
+        
+    },
+    forgotPasswordToken:String,
+    forgotPasswordTokenExpiry:Date,
+    verifyToken:String,
+    verifyTokenExpiry:Date
+
+},{timestamps:true})
+
+const User=mongoose.model.users|| mongoose.model("users",userSchema)
+
+export default User
